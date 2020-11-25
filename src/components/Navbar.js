e0947bdf056b891d905f13243197fe425404b9b3
Dane Santos <dsantos@umass.edu>
2020-11-25 05:32:06 -0500

    responsive web design for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react"
-import pdf from "../static/DANE_SANTOS_RESUME.pdf"
 
 function Navbar() {
     return (
@@ -8,7 +7,7 @@ function Navbar() {
             <ul>
                 <li className="nav-li"><a style={{textDecoration: "none", color: "white"}} href="#projects-anchor">Projects</a></li>
                 <li className="nav-li"><a style={{textDecoration: "none", color: "white"}} href="#start">Home</a></li>
-                <li className="nav-li"><a style={{textDecoration: "none" , color: "white"}} href="./DANE_SANTOS_RESUME.pdf" target="_blank">Resume</a></li>
+                <li className="nav-li"><a style={{textDecoration: "none" , color: "white"}} href="/Dane_Santos_Resume.png" target="_blank">Resume</a></li>
             </ul>
         </nav>
         </div>
